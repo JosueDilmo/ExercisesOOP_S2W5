c0387cc780ff4787d3af96cdd137c779d42beffb
josuedilmo <josuedilmo@gmail.com>
2022-02-17 14:00:29 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ExercisesOOP_S2W5.Question1;
+using System;
+
+namespace ExercisesOOP_S2W5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercises S2W5");
+            Console.WriteLine("Question .1");
+            Console.WriteLine("Question .2");
+            Console.WriteLine("Question .3");
+            Console.Write("Choose a question: ");
+            int menuOpt = Convert.ToInt32(Console.ReadLine());
+            switch (menuOpt)
+            {
+                case 1:
+                    Console.Write("Please, introduce the number to be multiplied: ");
+                    int userNumber = Convert.ToInt32(Console.ReadLine());
+                    MultiplicationTable table = new();
+                    table.multiplicationTable(userNumber);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    }
+}
---
+++ Question1/MultiplicationTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesOOP_S2W5.Question1
+{
+   public class MultiplicationTable
+    {
+        public void multiplicationTable(int userNumber)
+        {
+            for (int i = 2; i <= 10; i++)
+            {
+                int resultNumber = i * userNumber;
+                Console.WriteLine($"Multiplication result: {resultNumber}");
+            }
+        }
+    }
+}
